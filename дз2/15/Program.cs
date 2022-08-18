9591328cde626b55732ef90a45926b758d61f2ce
EvteevaTatyana <109299208+EvteevaTatyana@users.noreply.github.com>
2022-08-18 11:05:42 +0500

    Add files via upload

---
+++ дз2/15/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру,
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Введите номер недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+ if(number < 6)
+     {
+          Console.WriteLine("Рабочий день");
+     }
+ else if(number == 6)
+     {
+         Console.WriteLine("Суббота");
+     }
+ else if(number == 7)
+     {
+        Console.WriteLine("Воскресенье");
+     }
+Console.ReadKey();
